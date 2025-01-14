f529a8bd7383fb3ac201a448426f12133f1b9fbc
rafarencoretg <rafarencoretg@uc.cl>
2025-01-14 18:41:08 -0300

    CoastSat Idea

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
+import os
 import geopandas as gpd
+
 
 def transform_geojson_epsg(input_geojson, output_geojson, settings):
     """
@@ -36,11 +38,41 @@ def transform_geojson_epsg(input_geojson, output_geojson, settings):
     print(f"Archivo GeoJSON transformado guardado en: {output_geojson}")
 
 
-settings = {
+"""settings = {
     'output_epsg': 32718  # Cambia al EPSG deseado (ejemplo: WGS 84)
 }
 
 input_geojson = "CoastSat_Web\OHIGGINS\chi0356\chi0356_transects.geojson"
 output_geojson = "CoastSat_Web\OHIGGINS\chi0356\chi0356_transects_transformed.geojson"
 
-transform_geojson_epsg(input_geojson, output_geojson, settings)
+transform_geojson_epsg(input_geojson, output_geojson, settings)"""
+
+
+
+def convert_geojson_to_excel(geojson_folder):
+    """
+    Convierte todos los archivos .geojson de una carpeta a formato .xlsx y los guarda en otra carpeta.
+
+    :param geojson_folder: Ruta de la carpeta que contiene los archivos .geojson.
+    """
+
+    # Iterar sobre todos los archivos en la carpeta de entrada
+    for file_name in os.listdir(geojson_folder):
+        if file_name.endswith('.geojson'):
+            # Ruta completa del archivo de entrada
+            file_path = os.path.join(geojson_folder, file_name)
+
+            # Cargar el archivo .geojson
+            data = gpd.read_file(file_path)
+
+            # Convertir geometría a formato WKT
+            data['geometry'] = data['geometry'].apply(lambda x: x.wkt)
+
+            # Ruta completa del archivo de salida
+            output_path = os.path.join(geojson_folder, file_name.replace('.geojson', '.xlsx'))
+
+            # Guardar en formato Excel
+            data.to_excel(output_path, index=False)
+            print(f"Archivo convertido y guardado en: {output_path}")
+
+    print("Todos los archivos .geojson han sido procesados.")
---
+++ prueba_helper.py
@@ -0,0 +1,4 @@
+import helper
+
+
+helper.convert_geojson_to_excel("GIS")
